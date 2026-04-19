122e409b228d36dd72cdfb9486ed39ddd5a69786
Blank <blank@blankdev.me>
2026-04-19 18:02:10 -0400

    Fix str in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,25 +4,31 @@
 #include <thread>
 #include <chrono>
 #include <string>
-void showNotification(std::string seconds, std::string reason) {
-    std::string parsed = "Timer for " + reason + " has ended after " + seconds;
+
+
+void showNotification(int seconds, std::string type, std::string reason) {
+    std::string parsed = "Timer for " + reason + " has ended after " + std::to_string(seconds) + " " + type;
     std::string cmd = "zenity --info --title='Timer has ended' --text='" + parsed + "'";
     system(cmd.c_str());
 }
 
-int convertoseconds(std::string time) {
+int convertoseconds(std::string time, std::string* type) {
     std::string sub = time.substr(time.size() - 1);
     if (sub == "s") {
+        *type = "second";
         return std::stoi(time);
     } else if (sub == "m") {
+        *type = "minutes";
         return std::stoi(time) * 60;
     } else if (sub == "h") {
+        *type = "hours";
         return std::stoi(time) * 3600;
     } else {
         throw std::invalid_argument("Invalid time format. Use 's' for seconds, 'm' for minutes, or 'h' for hours.");
     }
 
 }
+
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " <reason> <duration>" << std::endl;
@@ -31,12 +37,14 @@ int main(int argc, char* argv[]) {
 
     std::string reason = argv[1];
     std::string duration = argv[2];
-
-    int time = convertoseconds(duration);
+    std::string type;
+    int time = convertoseconds(duration, &type);
+    int original_time = time;
     while (time > 0) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
         time--;
     }
-    showNotification(argv[2], reason);
+
+    showNotification(original_time, type, reason);
     return 0;
 }
